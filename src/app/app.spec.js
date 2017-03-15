57f4a0facd65dd7c1718b716f7f25535689db641
Igor Fedyukin <isfedyukin@gmail.com>
2017-03-15 16:33:01 +0300

    Test testing

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -31,4 +31,57 @@ describe('Items factory', () => {
       expect(todoStorage.items).toBeDefined();
     });
   });
+
+  describe('.removeItem()', () => {
+    it('should exist', function () {
+      expect(todoStorage.removeItem).toBeDefined();
+    });
+
+    it('should delete one item', function () {
+      todoStorage.items.push({
+        id: "tested",
+        text: "Test item",
+        checked: false
+      });
+
+      let length = todoStorage.items.length;
+      todoStorage.removeItem("tested");
+      expect(length - todoStorage.items.length).toBe(1);
+      expect(todoStorage.items).toBeDefined();
+    });
+  });
+
+  describe('.checkItem()', () => {
+    it('should exist', function () {
+      expect(todoStorage.checkItem).toBeDefined();
+    });
+
+    it('should check item', function () {
+      todoStorage.items.push({
+        id: "tested",
+        text: "Test item",
+        checked: false
+      });
+      todoStorage.checkItem("tested");
+
+      let length = todoStorage.items.length;
+      expect(todoStorage.items[length - 1].checked).toBe(true);
+      expect(todoStorage.items[length - 1].text).toBe("Test item");
+      expect(todoStorage.items).toBeDefined();
+    });
+
+    it('should uncheck item', function () {
+       todoStorage.items.push({
+        id: "tested",
+        text: "Test item",
+        checked: true
+      });
+      todoStorage.checkItem("tested");
+
+      let length = todoStorage.items.length;
+      expect(todoStorage.items[length - 1].checked).toBe(false);
+      expect(todoStorage.items[length - 1].text).toBe("Test item");
+      expect(todoStorage.items).toBeDefined();
+    });
+  });
 });
